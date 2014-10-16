bc2f060ded9fcac63350a17394acfd656ab5483c
lerenwe <johan.nagy.contact@gmail.com>
2014-10-16 15:54:16 +0200

    Working on soulmode
    Assez buggé, à cause du fait que le yiled waitforseconds déconne. A
    corriger, et à NETTOYER, c'est le bordel.

--- Assets/Scripts/Player/SoulMode/SoulMode.cs
+++ Assets/Scripts/Player/SoulMode/SoulMode.cs
@@ -6,32 +6,52 @@ public class SoulMode : MonoBehaviour {
 	float localDeltaTime;
 	float horizontal;
 	float vertical;
-	public float speed = 4;
+	float speed = 4;
 	CharacterController controller;
+	GameObject player;
 	PlayerController playerScript;
+	int currentGhostNumber = 0;
 
 	ThirdPersonCamera mainCameraScript;
+	BirdsEyeCam birdsEyeScript;
+	public GameObject ghostPrefab;
+
+	Vector3 direction = Vector3.zero;
+	Vector3 tempMoveDir;
+	Vector3 moveDirection = Vector3.zero;
+	Vector3 faceDirection = Vector3.zero;
+	float floatDir = 0f;
+	public float maxSpeed = 5;
 
 	// Use this for initialization
 	void Start () {
 		this.name = "Soul";
 		mainCameraScript = Camera.main.GetComponent<ThirdPersonCamera> ();
+		birdsEyeScript = Camera.main.GetComponent<BirdsEyeCam> ();
 		mainCameraScript.birdsEyeActivated = true;
-		playerScript = GameObject.FindWithTag ("Player").GetComponent<PlayerController> ();
-
+		birdsEyeScript.followBody = false;
+		player = GameObject.FindWithTag ("Player");
+		playerScript = player.GetComponent<PlayerController> ();
 		controller = this.GetComponent<CharacterController> ();
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
+		Time.timeScale = 0.1f;
+		Time.fixedDeltaTime = 0.1f * 0.02f; //Make sure the physics simulation is still fluid.
+
 		//localDeltaTime allows the script to not be influenced by the time scale change.
 		localDeltaTime = (Time.timeScale == 0) ? 1 : Time.deltaTime / Time.timeScale;
 
+
+		//Resetting back to body mode...
 		if (Input.GetButtonDown ("SwitchMode")) 
 		{
-			mainCameraScript.birdsEyeActivated = false;
-			playerScript.soulMode = false;
+			player.GetComponent<ghostFollow>().enabled = true;
+			player.GetComponent<ghostFollow>().justGotActivated = true;
+			currentGhostNumber = 0;
+
 			Destroy (this.gameObject);
 		}
 
@@ -41,8 +61,21 @@ public class SoulMode : MonoBehaviour {
 		vertical = Input.GetAxis ("Vertical");
 		#endregion
 
-		controller.Move (transform.forward * vertical * speed * localDeltaTime);
-		controller.Move (transform.right * horizontal * speed * localDeltaTime);
+		//This method will translate axis input into world coordinates, according to the camera's point of view.
+		stickToWorldSpace(transform, mainCameraScript.transform, ref direction, ref floatDir, ref speed, false);
+
+		Quaternion target = Quaternion.Euler(0, floatDir, 0);
+		tempMoveDir = target * Vector3.forward * speed;
+		tempMoveDir = transform.TransformDirection (tempMoveDir * maxSpeed); 
+		moveDirection.x = tempMoveDir.x;
+		moveDirection.z = tempMoveDir.z;
+
+		controller.Move(moveDirection * localDeltaTime);
+		
+		faceDirection = transform.position + moveDirection;
+		faceDirection.y = transform.position.y;
+		
+		transform.LookAt (faceDirection);
 
 		if(Input.GetButtonDown("Action"))
 			placeGhost();
@@ -52,8 +85,42 @@ public class SoulMode : MonoBehaviour {
 	{
 		RaycastHit pointHit;
 		if (Physics.Raycast (transform.position, -transform.up,out pointHit, 50)) {
-		
+			GameObject placedGhost = Instantiate (ghostPrefab, pointHit.point + new Vector3(0,1,0), this.transform.rotation) as GameObject;
+			placedGhost.name = "actionGhost_"+currentGhostNumber;
+			currentGhostNumber++;
 		}
+	}
+
+	public void stickToWorldSpace(Transform root, Transform camera, ref Vector3 directionOut, ref float floatDirOut, ref float speedOut, bool outForAnim)
+	{
+		//We take the model's direction, the stick's direction, then we put in the square magnitude.
+		Vector3 rootDirection = root.forward;
+		Vector3 stickDirection = new Vector3(horizontal, 0, vertical);
+		speedOut = stickDirection.sqrMagnitude;
+		
+		//Getting the camera's current rotation.
+		Vector3 CameraDirection = camera.forward;
+		CameraDirection.y = 0.0f;
+		Quaternion referentialShift = Quaternion.FromToRotation (Vector3.forward, CameraDirection);
+		
+		//Conversion de l'input du joystick/clavier en coordonnées World.
+		Vector3 moveDirection = referentialShift * stickDirection;
+		Vector3 axisSign = Vector3.Cross(moveDirection, rootDirection);
+		
+		#region debug draw rays
+		//Ces lignes permettent de visualiser la façon dont sont gérés les vecteurs dans la fonction StickToWorldSpace (Debug)
+		
+		/*Debug.DrawRay (new Vector3(root.position.x, root.position.y + 2f, root.position.z), moveDirection, Color.green);
+		Debug.DrawRay (new Vector3(root.position.x, root.position.y + 2f, root.position.z), axisSign, Color.red);
+		Debug.DrawRay (new Vector3(root.position.x, root.position.y + 2f, root.position.z), rootDirection, Color.magenta);
+		Debug.DrawRay (new Vector3(root.position.x, root.position.y + 2f, root.position.z), stickDirection, Color.blue);*/
+		#endregion
+		
+		//Give the angle between the model's direction and the direction we give to it.
+		float angleRootToMove = Vector3.Angle (rootDirection, moveDirection) * (axisSign.y >= 0 ? -1f :1f);
+		
+		//DirectionOut will be useful to give the direction where the character have to go to the animator.
+		floatDirOut = angleRootToMove;
 	}
 
 }
---
+++ Assets/ghostFollow.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ghostFollow : MonoBehaviour {
+
+
+	PlayerController controllerScript;
+	int currentTargetedGhostNumber = 0;
+	GameObject currentTargetedGhost;
+	CharacterController controller;
+	float localDeltaTime;
+	ThirdPersonCamera mainCameraScript;
+	BirdsEyeCam birdsEyeScript;
+
+	public float speed = 10f;
+	bool canGoToNext = true;
+
+	[HideInInspector]
+	public bool justGotActivated = false;
+
+	// Use this for initialization
+	void Start () {
+		controllerScript = this.GetComponent<PlayerController> ();
+		controller = this.GetComponent<CharacterController> ();
+		mainCameraScript = Camera.main.GetComponent<ThirdPersonCamera> ();
+		birdsEyeScript = Camera.main.GetComponent<BirdsEyeCam> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+
+						//localDeltaTime allows the script to not be influenced by the time scale change.
+						localDeltaTime = (Time.timeScale == 0) ? 1 : Time.deltaTime / Time.timeScale;
+
+						//First, if there's at least one ghost, let's target it.
+
+						if (GameObject.FindGameObjectWithTag ("Action Ghost") != null) {
+
+								if (justGotActivated) {
+										currentTargetedGhost = GameObject.Find ("actionGhost_" + currentTargetedGhostNumber);
+										birdsEyeScript.followBody = true;
+										justGotActivated = false;
+								}
+						if (canGoToNext) {
+								Vector3 GhostPos = currentTargetedGhost.transform.position;
+								//Then, let's make Phalene look into it's direction, now.
+								transform.LookAt (new Vector3 (GhostPos.x, transform.position.y, GhostPos.z));
+
+								//And let's go forward into that direction, speed of light, to infinity and beyond !
+				controller.Move (transform.forward * speed * localDeltaTime); }
+						} else {	
+								controllerScript.soulMode = false;
+								currentTargetedGhostNumber = 0;
+								Time.timeScale = 1;
+								Time.fixedDeltaTime = .02f;
+								mainCameraScript.birdsEyeActivated = false;
+								birdsEyeScript.followBody = false;
+								this.GetComponent<ghostFollow> ().enabled = false;
+						}
+				}
+
+	void OnTriggerEnter(Collider collider)
+	{
+		Debug.Log (collider.name);
+		if (collider.CompareTag ("Action Ghost")) 
+		{
+			canGoToNext = false;
+			waitBeforeNextGhost(collider);
+		}
+	}
+
+	IEnumerator waitBeforeNextGhost(Collider collider)
+	{
+		Destroy (collider.gameObject);
+		currentTargetedGhostNumber ++;
+		yield return new WaitForSeconds (.5f);
+		Debug.Log ("Waited");
+		if (GameObject.FindGameObjectWithTag ("Action Ghost") != null) {
+		currentTargetedGhost = GameObject.Find ("actionGhost_" + currentTargetedGhostNumber);
+	}
+		canGoToNext = true;
+	}
+
+}
