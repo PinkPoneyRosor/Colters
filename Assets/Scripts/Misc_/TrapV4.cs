f49db12bd2cee65773aa3b2d866ee58c08d79999
Stinkhole <quentin.jeanneret@gmail.com>
2015-02-25 15:58:35 +0100

    Début Map prison

---
+++ Assets/Scripts/Misc_/TrapV4.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapV4 : MonoBehaviour {
+
+	public Animation animation;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+	/*void OnTriggerEnter(Collider c)
+	{
+		if (c.gameObject.tag == "ThrowableRock")
+		{
+			animation.Play();
+		}
+
+}*/
+}
