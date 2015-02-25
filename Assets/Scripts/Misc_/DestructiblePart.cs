a55cc6d60c9fe3032bf84deeb7b6c1e51066025f
Stinkhole <quentin.jeanneret@gmail.com>
2015-02-25 09:50:48 +0100

    G mis 1 script pr les éléments kon détruit

---
+++ Assets/Scripts/Misc_/DestructiblePart.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestructiblePart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.tag == "ThrowableRock") {
+			
+			rigidbody.constraints = RigidbodyConstraints.None;
+	
+
+	
+
+			
+		}
+	}
+
+}
