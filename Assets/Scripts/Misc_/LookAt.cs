1d3a02f57e12982e3e37a582a7fd25a9611dfe74
Stinkhole <quentin.jeanneret@gmail.com>
2015-02-26 13:01:10 +0100

    Partie 1 (Prison) Finie

---
+++ Assets/Scripts/Misc_/LookAt.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAt : MonoBehaviour {
+
+
+	public Transform Player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt(Player);
+	}
+
+	
+	/*void OnTriggerEnter(Collider c)
+	{
+		if (c.gameObject.tag == "Player")
+		{
+			transform.LookAt(Player);
+		}
+		
+	}*/
+
+}
