420a3b0d6e72b7ade278eb320c2d83de0c35a303
Stinkhole <quentin.jeanneret@gmail.com>
2015-04-07 15:53:12 +0200

    Les flèches

---
+++ Assets/Scripts/Enemies/Projectile.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+	public int degats = 1;
+	public float speed = 10;
+	public GameObject explosionModelNormal;
+	public GameObject explosionModelTracking; 
+	public GameObject newExp;
+
+	public Transform explosionParent;
+	
+	public enum projectileType { Normal = 0, Tracking = 1};
+	public projectileType type = projectileType.Normal;
+	
+	public Transform target;
+
+	void  Start ()
+	{
+
+		if (explosionParent == null)
+		{
+			GameObject root = GameObject.Find("Level/Explosions");
+			
+			if (root != null)
+			{
+				explosionParent = root.transform;
+			}
+		}
+		
+		switch (type)
+		{
+		case projectileType.Normal:
+
+			StartCoroutine ("DoSomething");
+			break;
+
+			
+		case projectileType.Tracking:
+			
+			StartCoroutine("DoSomethingElse");
+			break;
+
+			
+		default:
+			break;
+		}
+	}
+	
+	void  Update (){
+		Vector3 direction;
+		
+		switch (type)
+		{
+		case projectileType.Normal:
+			break;
+			
+		case projectileType.Tracking:
+			if (target != null)
+			{
+				this.transform.LookAt (new Vector3 (target.position.x, target.position.y+0.5f, target.position.z) );
+			}
+			break;
+			
+		default:
+			break;
+		}
+		direction = transform.forward;
+		transform.position += direction * (speed * Time.deltaTime);
+	}
+	
+	void  OnTriggerEnter ( Collider other  )
+	{
+		other.SendMessage("OnHit", degats, SendMessageOptions.DontRequireReceiver);
+		Destroy(gameObject);
+		switch (type)
+		{
+		case projectileType.Normal:
+			
+			//Destroy(gameObject);
+			newExp = Instantiate(explosionModelNormal, transform.position, Quaternion.identity) as GameObject;
+			//newExp.transform.parent = explosionParent;
+			break;
+			
+		case projectileType.Tracking:
+			
+			//Destroy(gameObject);
+			newExp = Instantiate(explosionModelTracking, transform.position, Quaternion.identity) as GameObject;
+			//newExp.transform.parent = explosionParent;
+			break;
+			
+		default:
+			break;
+		}
+		newExp.transform.parent = explosionParent;
+	}
+	
+	public void  SetTarget (  Transform newTarget  ){
+		target = newTarget;
+		transform.LookAt(new Vector3(target.position.x, target.position.y+0.5f, target.position.z));
+	}
+
+
+	IEnumerator DoSomething() {
+		yield return new WaitForSeconds (3);
+		Destroy(gameObject);
+		newExp = Instantiate(explosionModelNormal, transform.position, Quaternion.identity) as GameObject;
+		newExp.transform.parent = explosionParent;
+	}
+
+	IEnumerator DoSomethingElse() {
+		yield return new WaitForSeconds (6);
+		Destroy(gameObject);
+		newExp = Instantiate(explosionModelTracking, transform.position, Quaternion.identity) as GameObject;
+		newExp.transform.parent = explosionParent;
+	}
+
+
+
+
+
+
+
+
+}
---
+++ Assets/Scripts/Enemies/Tower.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tower : MonoBehaviour {
+
+	public float range = 5;
+	public GameObject projectileModel;
+	public float FireRate = 0.2f;
+	public Transform projectileSpawn; 
+	public Transform projectileParent;
+
+	public GameObject player;
+	
+	void  Start (){
+		if (projectileParent == null)
+		{
+			GameObject root = GameObject.Find("Level/Projectiles");
+			
+			if (root != null)
+			{
+				projectileParent = root.transform;
+			}
+		}
+
+		player = GameObject.Find("Player");
+	}
+	
+	public float nextProjectile = 0;
+	public int numProjectile = 0;
+	
+	void  Update ()
+	{	
+		float sqrRange = range*range;
+		Vector3 diff;
+		Vector3 phalenePosition;
+		GameObject phalene = GameObject.FindGameObjectWithTag("Player") as GameObject;
+		float nearDistance = 1000000;
+		GameObject nearPhalene = null;
+
+
+			phalenePosition = phalene.transform.position;
+			phalenePosition.y = 0;
+			diff = phalenePosition - transform.position;
+			
+			if (diff.sqrMagnitude < sqrRange)
+			{
+			Debug.Log ("Coucou1");
+				float distanceBetweenPlayerAndTower = Vector3.SqrMagnitude (this.transform.position - player.transform.position);
+				
+				if (distanceBetweenPlayerAndTower < nearDistance)
+				{
+				Debug.Log ("Coucou3");
+					nearDistance = distanceBetweenPlayerAndTower;
+					nearPhalene = phalene;
+				}
+			}
+		if (nearPhalene != null)
+		{
+			Debug.Log ("Coucou2");
+			phalenePosition = nearPhalene.transform.position;
+			phalenePosition.y = transform.position.y;
+			transform.LookAt(phalenePosition);
+			
+			if (Time.time >= nextProjectile)
+			{
+				GameObject newProj = Instantiate(projectileModel, projectileSpawn.position, Quaternion.identity) as GameObject;
+				
+				newProj.name = "Projectile_" + gameObject.name + "_" + numProjectile++;
+				newProj.transform.parent = projectileParent;
+				newProj.GetComponent<Projectile>().SetTarget(nearPhalene.transform);
+				nextProjectile = Time.time + FireRate;
+			}
+			
+		}
+	}
+	
+	void  OnDrawGizmosSelected (){
+		Gizmos.color = Color.cyan;
+		Gizmos.DrawWireSphere(transform.position, range);
+	}
+}
