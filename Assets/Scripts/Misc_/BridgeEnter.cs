eebaf5f578db37574246286ef81f49c27eb5202d
Stinkhole <quentin.jeanneret@gmail.com>
2015-05-05 09:39:20 +0200

    Some Improvements

---
+++ Assets/Scripts/Misc_/BridgeEnter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BridgeEnter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	
+	void OnTriggerEnter(Collider c)
+	{
+		if (c.gameObject.tag == "Player")
+		{
+			this.animation.Play("AnimationPontPetitCube");
+			Debug.Log("Le pont !");
+		}
+	}
+
+}
--- Assets/Scripts/Misc_/FallingBridge.cs
+++ Assets/Scripts/Misc_/FallingBridge.cs
@@ -7,10 +7,12 @@ public class FallingBridge : MonoBehaviour {
 	
 	public float speed = 5;
 	public bool falling = false;
+	public bool soulIsHere = false;
 
 	// Use this for initialization
 	void Start () {
-	
+		
+		rigidbody.constraints = RigidbodyConstraints.FreezeAll;
 	}
 	
 	void OnTriggerEnter(Collider c){
@@ -18,6 +20,16 @@ public class FallingBridge : MonoBehaviour {
 		if(c.gameObject.tag == "Player")
 		{
 			falling = true;
+			rigidbody.constraints = RigidbodyConstraints.None;
+			StartCoroutine ("GottaGetDestroyed");
+		}
+
+		if(c.gameObject.tag == "PlayerSoul")
+		{
+	
+			falling = true;
+			soulIsHere = true;
+			rigidbody.constraints = RigidbodyConstraints.None;
 		}
 	}
 	
@@ -27,4 +39,13 @@ public class FallingBridge : MonoBehaviour {
 			transform.Translate(Vector3.down * speed * Time.deltaTime);
 		}
 	}
+
+	IEnumerator GottaGetDestroyed ()
+	{
+		yield return new WaitForSeconds (1);
+		Destroy(this.gameObject);
+	}
+
+
+
 }
