b2b330cfb05d94193c9a476f4f1a2e917e54471c
Stinkhole <quentin.jeanneret@gmail.com>
2015-05-22 16:19:23 +0200

    up to date I think

---
+++ Assets/Scripts/Misc_/PassageAvantFinal.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PassageAvantFinal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider c)
+	{
+		if (c.gameObject.tag == "ThrowableRock")
+		{
+			this.animation.Play("PassageAvantFinal");
+			Debug.Log("Play!");
+		}
+	}
+
+
+}
