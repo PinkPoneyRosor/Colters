7939e4246315a136d4edb9cf9a9e94024f873818
lerenwe <johan.nagy.contact@gmail.com>
2015-06-12 10:36:19 +0200

    Some... Eurgh??? I dunno.

--- Assets/Scripts/Camera/Camera 1/CameraTrigger1.cs
+++ Assets/Scripts/Camera/Camera 1/CameraTrigger1.cs
@@ -10,6 +10,8 @@ public class CameraTrigger1 : MonoBehaviour {
 	private GameObject player;
 	private PlayerController playerScript; 
 	private GameObject soul;
+	
+	private bool alreadyTriggered = false;
 
 	// Use this for initialization
 	void Start () 
@@ -29,12 +31,14 @@ public class CameraTrigger1 : MonoBehaviour {
 
 	void OnTriggerEnter(Collider c)
 	{
-		if (c.gameObject.tag == "Player" || c.gameObject.tag == "PlayerSoul")
+		if ( (c.gameObject.tag == "Player" || c.gameObject.tag == "PlayerSoul") && !alreadyTriggered)
 		{
 			mainGameCamera.animation.Play("Camera1");
 			Debug.Log("Play Cam!");
 			HUDScript.paused = true;
 			HUD.GetComponent <Canvas>().enabled = false;
+			
+			alreadyTriggered = true;
 			
 			if (playerScript.soulMode)
 			{
