aee68baba76ecd48f7074413ff14726d717ccc89
Lerenwe <johan.nagy.contact@gmail.com>
2015-07-02 18:06:29 +0200

    ???

---
+++ Assets/MenuAssets/CreditsDefil.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CreditsDefil : MonoBehaviour {
+
+	public Image Credits;
+	public bool go = false;
+
+	private float localDeltaTime;
+	private Vector2 startPos;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//Credits.rectTransform.position = new Vector2 (Screen.width / 2, -Screen.height - 20);
+		startPos = Credits.rectTransform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		localDeltaTime = (Time.timeScale == 0) ? 1 : Time.deltaTime / Time.timeScale;
+
+		if (go)
+		{
+			Credits.rectTransform.position += Vector3.up * 150 * localDeltaTime;
+			Debug.Log ("Gogolol");
+		}
+
+		if (Credits.rectTransform.position.y > Screen.height + Credits.rectTransform.rect.height * 1.5f)
+		{
+			go = false;
+			Application.LoadLevel(0);
+		}
+	}
+
+	public void GoGoGo ()
+	{
+		go = true;
+	}
+}
